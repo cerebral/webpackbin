9d9b67c6efffdce17a4d58c597241f796583790e
Christian Alfoni <christianalfoni@gmail.com>
2017-02-20 11:18:47 +0100

    Update sandbox on region change

--- src/modules/settings/signals/regionToggled.js
+++ src/modules/settings/signals/regionToggled.js
@@ -1,8 +1,11 @@
 import {set} from 'cerebral/operators'
 import {state, props} from 'cerebral/tags'
 import updateFirebaseSettings from '../factories/updateFirebaseSettings'
+import updateSandbox from 'modules/sandbox/factories/updateSandbox'
 
 export default [
   set(state`settings.region`, props`region`),
-  ...updateFirebaseSettings('region')
+  ...updateSandbox([
+    ...updateFirebaseSettings('region')
+  ])
 ]
